cc1f59f2086470b84db13b142fa7eafa1aece5d6
Damilola <dami_fagbo@yahoo.com>
2021-03-19 13:36:57 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
+//The onmouseover event should use classlist to add a style to an element.
+//The onmouseout event should use classlist to remove a style to an element.
 
-
- 
- let numberArray = [1,2,4,9,0,8,24,19]
-
-
-//Use the filter method to iterate over each element and pass to the function
-   let arrays = numberArray.filter(function(arr){
-        //filter through to check through the array to see the number divisible by 2 without remainder and return
-          return  arr % 2 == 0;
-     
-    });
-  console.log(arrays);
     
+ const button = document.querySelector('.button')
+ 
 
+//on mouseover
+function over(){
+   button.classList.add("items");  
+};
+
+//onmouseout
+function out(){  
+    button.classList.remove('items');
+    };
 
